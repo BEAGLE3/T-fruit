123a07312a7fee144b5efdf73c6da21d834389e6
BEAGLE3 <71073090+BEAGLE3@users.noreply.github.com>
2021-01-15 15:04:04 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -11,6 +11,9 @@ var fruits;
 var fruitGroup;
 var fruit1_img, fruit2_img, fruit3_img, fruit4_img, fruit5_img;
 var player_img;
+var player1score=0;
+var player2score=0;
+
 
 function preload(){
   back_img = loadImage("images/jungle.jpg");
